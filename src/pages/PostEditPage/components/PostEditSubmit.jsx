bf9181d82e0a3b9f77476dffbe1723238f589428
Stella <yeobinlee1210@gmail.com>
2026-08-04 21:54:24 +0900

    feat(PostEditSubmit): 게시물 수정 로직 수정

--- src/pages/PostEditPage/components/PostEditSubmit.jsx
+++ src/pages/PostEditPage/components/PostEditSubmit.jsx
@@ -1,46 +1,67 @@
-import { useNavigate } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
 import { requestCsrfAPIJsonResponse } from "../../../api/csrf";
 export const PostEditSubmit = ({isPostEnable, title, content, file}) =>{
     const navigate = useNavigate();
+    const [isSubmitting, setIsSubmitting] = useState(false);
     const postId = useParams()?.postId;
 
+    function makePostFormData(){
+        const formData = new FormData();
+        formData.append(
+            "request",
+            new Blob(
+                [
+                    JSON.stringify({
+                        title: title,
+                        content: content
+                    }),
+                ],
+        { type: "application/json" }
+        )
+        );
+
+        if (file instanceof File) {
+            formData.append("file", file);
+        }
+       return formData;
+    
+    }
+    
     async function handleSubmitPost(){
-        const csrf = await requestCsrfAPIJsonResponse();
-        
         try{
+            const csrf = await requestCsrfAPIJsonResponse();
+            const formData =makePostFormData();
+            setIsSubmitting(true);
             const response = await fetch(`/posts/${postId}`, {
                 method: 'PATCH',
                 credentials:"include",
                 headers: {
-                    'Content-Type': 'application/json',
                     [csrf.headerName] : csrf.token
         
                 },
-                body: JSON.stringify({
-                    title : title,
-                    content : content,
-                    file : file 
-                })
+                body: formData
             });
         
             if (response.status === 401) {
                 navigate("/login")
+                return;
             }
         
             if (!response.ok) {
-                throw new Error('게시글 작성 실패');
+                throw new Error('게시글 수정 실패');
             }
         
-            const data = await response.json();
+            setIsSubmitting(false)
             navigate('/board');
         }catch(error){
             console.error('오류 발생:', error);
         }
     }
+
     return (
         <>
-            <button id="postWriteBtn" type="button" disabled={!isPostEnable} onClick={handleSubmitPost}>완료</button>
+            <button id="postWriteBtn" type="button" disabled={!isPostEnable || isSubmitting} onClick={handleSubmitPost}>{isSubmitting ? "수정 중..." : "완료"}</button>
         </>
     )
 }
