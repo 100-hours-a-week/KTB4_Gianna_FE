9cb036a02f620eb2d16c9de427b3152a562646c4
Stella <yeobinlee1210@gmail.com>
2026-08-07 21:19:36 +0900

    feat(MyPostPage): 내 게시글 페이지 생성

---
+++ src/pages/MyPostPage/MyPostPage.jsx
@@ -0,0 +1,16 @@
+import './MyPostPage.css';
+import { Header } from '../../components/Header/Header.jsx';
+
+export const MyPostPage = () => {
+    return (
+        <>
+            <div id="headerContainer">{<Header />}</div>
+            <main className="my-post-page">
+                <section className="my-post-box">
+                    <h1>내 게시물</h1>
+                    <p>내가 작성한 게시물이 이곳에 모입니다.</p>
+                </section>
+            </main>
+        </>
+    );
+};
