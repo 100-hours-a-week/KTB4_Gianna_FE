914823da466b2db450a03553f07082b3c8459485
Stella <yeobinlee1210@gmail.com>
2026-08-09 19:41:45 +0900

    fix(PostWritePage): 게시물 작성 css 수정

--- src/pages/PostWritePage/components/PostWriteForm.jsx
+++ src/pages/PostWritePage/components/PostWriteForm.jsx
@@ -1,11 +1,11 @@
 export const PostWriteForm = ({onTitleChange, onContentChange, onFileChange}) =>{
     return (
         <div className="post-write-container">
-            <label htmlFor="postTitle">제목*</label>
-            <input id="postTitle" type="text" maxLength="26" placeholder="제목을 입력해주세요. (최대 26글자)"  onChange={(event)=>{onTitleChange(event.target.value)}}></input>
+            <label htmlFor="postWriteInputTitle">제목*</label>
+            <input id="postWriteInputTitle" type="text" maxLength="26" placeholder="제목을 입력해주세요. (최대 26글자)"  onChange={(event)=>{onTitleChange(event.target.value)}}></input>
 
-            <label htmlFor="postContent">내용*</label>
-            <textarea id="postContent" placeholder="내용을 입력해주세요." onChange={(event)=>{onContentChange(event.target.value)}}></textarea>
+            <label htmlFor="postWriteInputContent">내용*</label>
+            <textarea id="postWriteInputContent" placeholder="내용을 입력해주세요." onChange={(event)=>{onContentChange(event.target.value)}}></textarea>
             <p id="helperText" className="helper-text"></p>
 
             <label htmlFor="postUploadedPicture">이미지</label>
