2e044425cb54201678d6d6385807f4a5be528605
Stella <yeobinlee1210@gmail.com>
2026-08-09 19:06:51 +0900

    fix(MyPostPage): 공지 추가

--- src/pages/MyPostPage/MyPostPage.jsx
+++ src/pages/MyPostPage/MyPostPage.jsx
@@ -6,6 +6,7 @@ export const MyPostPage = () => {
         <>
             <div id="headerContainer">{<Header />}</div>
             <main className="my-post-page">
+                <h1>현재 개발 중입니다... 곧 만나요!</h1>
                 {/* <section className="my-post-box">
                     <h1>내 게시물</h1>
                     <p>내가 작성한 게시물이 이곳에 모입니다.</p>
