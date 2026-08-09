647f2d6a118a25eea55eff4e3ef17906f939d892
Stella <yeobinlee1210@gmail.com>
2026-08-09 17:55:06 +0900

    chore: 웹소켓 프록시 추가

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         changeOrigin: true,
         },
       '/posts': 'http://localhost:8080',
+      '/discussion/messages': 'http://localhost:8080',
       '/login-process': 'http://localhost:8080',
       '/logout': 'http://localhost:8080',
       '/csrf': 'http://localhost:8080',
